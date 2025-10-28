496e0d9b1db244ed5092708bf91556673d25d441
Tr. Thanh <ltrthanh.dev@gmail.com>
2025-10-28 15:07:42 +0700

    [PR] require_numeric_field: relase version 18.1.0.0 (#5)
    * [IMP] require_numeric_field: add define context skip_check_requirement to bypass checking required for numeric fields
    
    * [REF] require_numeric_field: adjust `index.html`.

--- require_numeric_field/static/src/model/relation_model/record.js
+++ require_numeric_field/static/src/model/relation_model/record.js
@@ -4,18 +4,21 @@ import { patch } from "@web/core/utils/patch";
 import { markup } from "@odoo/owl";
 import { escape } from "@web/core/utils/strings";
 import { _t } from "@web/core/l10n/translation";
+import { evaluateExpr } from "@web/core/py_js/py";
 
 patch(Record.prototype, {
     _checkValidity({ silent, displayNotification, removeInvalidOnly } = {}) {
         for (const fieldName in this.activeFields) {
+            const activeField = this.activeFields[fieldName];
             const fieldType = this.fields[fieldName].type;
+            let skip_check_requirement =  false;
             if (this._isInvisible(fieldName) || this.fields[fieldName].relatedPropertyField) {
                 continue;
             }
             if (["float", "integer"].includes(fieldType)) {
                 const val = this.data[fieldName];
-                console.log(fieldName, ':', this.activeFields[fieldName].value)
-                if (this._isRequired(fieldName) && val === 0) {
+                skip_check_requirement = evaluateExpr(activeField.context, this.evalContextWithVirtualIds).skip_check_requirement || false;
+                if (this._isRequired(fieldName) && val === 0 && !skip_check_requirement) {
                     this._setInvalidField(fieldName);
                 }
             }
